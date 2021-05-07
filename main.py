7171bc81fd499276bbb5e9e6160a09c135ca7dbf
A01705746 <65366693+A01705746@users.noreply.github.com>
2021-05-06 21:01:42 -0500

    Esqueleto de menú

--- main.py
+++ main.py
@@ -6,11 +6,41 @@ import matplotlib.pyplot as plt
 if __name__ == '__main__':
 
     #Se obtiene la imagen con cv2
-    print("Escribe el nombre de una imagen que este en la carpeta del proyecto:")
+    print("Escribe el nombre de una imagen con su extensión que esté en la carpeta del proyecto:")
     image = cv2.imread(input())
-    RGB_img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) #convierte el espectro de color a RGB
+    RGB_img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) #convierte el espectro de color a RGB.
 
-    #Impresion de imagen
-    plt.imshow(RGB_img, cmap='gray')
-    plt.title("Imagen")
+    # Muestra la imagen original por un plot.
+    plt.imshow(RGB_img)
+    plt.title("Imagen Original")
     plt.show()
+
+    # Menu
+    filtros = """
+Aplicar filtro
+1.- Filtro 1 - David
+2.- Filtro 2 - Enrique
+3.- Filtro 3 - Uriel
+4.- Cambiar de imagen
+5.- Salir                 
+            """
+
+    pop = True #Breack del ciclo while.
+    while pop == True: #Ciclo while para imprimir filtros, cambiar imagen o salir.
+        print(filtros)
+        print("Elige una opcion:")
+        op = input()
+
+        if op == "1":
+            print('Filtro David')
+        elif op == "2":
+            print('Filtro Enrique')
+        elif op == "3":
+            print('Filtro Uriel')
+        elif op == "4":
+            print('Cambio de imagen')
+        elif op == "5":
+            pop = False
+        else:
+            print("Opción inválida")
+    #Termina el ciclo while
